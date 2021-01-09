367b033ee00576113d9b21bf291b34f0eb41bca4
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2021-01-09 13:49:20 -0500

    Models were created without foreing kewys due to "' 'tuple' object has no attribute 'foreign_keys' " error.

--- Medical_record_management/models.py
+++ Medical_record_management/models.py
@@ -1,17 +1,11 @@
 from datetime import datetime
-from abc import ABCMeta
 
 from .database import db
 
 
-hospital_service = ('HospitalService',
-    db.Column('pk', db.Integer, primary_key=True),
-    db.Column('hospital_pk', db.Integer, db.ForeignKey('hospital.pk')),
-    db.Column('service_pk', db.Integer, db.ForeignKey('service.pk'))
-)
-
 class Hospital(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
     identification = db.Column(db.String(10), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(10), nullable=False)
@@ -21,36 +15,33 @@ class Hospital(db.Model):
     user_type = db.Column(db.String(10), nullable=False)
     is_activated = db.Column(db.Boolean, default=True)
     is_verificated = db.Column(db.Boolean, default=False)
-    services = db.relationship('Service', secondary=hospital_service, lazy='dynamic')
-    doctors = db.relationship('Doctor', backref='doctor')
-    medical_registers = db.relationship('MedicalRegister', backref='medical_register')
 
 class Service(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     service_name = db.Column(db.String(20), nullable=False)
 
+class HospitalService(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hospital_id = db.Column(db.Integer, nullable=False)
+    service_id = db.Column(db.Integer, nullable=False)
+
 class Patient(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
     identification = db.Column(db.String(10), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     phone = db.Column(db.String(10), nullable=False)
     user_type = db.Column(db.String(10), nullable=False)
     is_activated = db.Column(db.Boolean, default=True)
     is_verificated = db.Column(db.Boolean, default=False)
-    birth_day = db.Column(db.DateTime, nullable=False)
-    medical_registers = db.relationship('MedicalRegister', backref='medical_register')
-
-doctor_speciality = ('DoctorSpeciality',
-    db.Column('pk', db.Integer, primary_key=True),
-    db.Column('doctor_pk', db.Integer, db.ForeignKey('doctor.pk')),
-    db.Column('medical_speciality_pk', db.Integer, db.ForeignKey('medicalspeciality.pk'))
-)
+    birth_day = db.Column(db.String(10), nullable=False)
 
 class Doctor(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
     identification = db.Column(db.String(10), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(10), nullable=False)
@@ -61,19 +52,22 @@ class Doctor(db.Model):
     is_activated = db.Column(db.Boolean, default=True)
     is_verificated = db.Column(db.Boolean, default=False)
     password_changed = db.Column(db.Boolean, default=False)
-    hospital_pk = db.Column(db.Integer, db.ForeignKey('hospital.pk'))
-    specialities = db.relationship('MedicalSpeciality', secondary=doctor_speciality)
-    medical_registers = db.relationship('MedicalRegister', backref='medical_register')
+    hospital_id = db.Column(db.Integer, nullable=False)
 
 class MedicalSpeciality(db.Model):
-    pk = db.Column(db.String(10), primary_key=True)
-    speciality_name = db.Column(db.String(20), nullable=False) 
+    id = db.Column(db.String(10), primary_key=True)
+    speciality_name = db.Column(db.String(20), nullable=False)
+
+class DoctorSpeciality(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    doctor_id = db.Column(db.Integer, nullable=False)
+    medical_speciality_id = db.Column(db.Integer, nullable=False)
 
 class MedicalRegister(db.Model):
-    pk = db.Column(db.Integer, primary_key=True)
-    patient_pk = db.Column(db.Integer, db.ForeignKey('patient.pk'))
-    doctor_pk = db.Column(db.Integer, db.ForeignKey('doctor.pk'))
-    hospital_pk = db.Column(db.Integer, db.ForeignKey('hospital.pk'))
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, nullable=False)
+    doctor_id = db.Column(db.Integer, nullable=False)
+    hospital_id = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, default=datetime.now) 
     medical_speciality = db.Column(db.String(20), nullable=False)
     description = db.Column(db.String(10000), nullable=False)
