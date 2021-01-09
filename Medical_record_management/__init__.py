ffe4d57c4ca80b12ee74f7392ea81e31aac8bf3d
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2021-01-09 10:26:14 -0500

    Flask app was created and the database was configured.

---
+++ Medical_record_management/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+from .app.routes import auth_app
+from .database import db
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET_KEY'] = 'a-18c3%!*qhz4_bs1-nt7yb2fq8xlv8d_7cyx^5i-o+qqmty@'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Manuel:1234@localhost/Medical_record_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    app.register_blueprint(auth_app)
+    
+    return app
