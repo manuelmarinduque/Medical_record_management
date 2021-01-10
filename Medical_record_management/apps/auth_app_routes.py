1f1753d6e0c1ed34f8b29d7d8689126ad2bf707a
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2021-01-10 13:23:48 -0500

    Some fixes were done.

--- Medical_record_management/apps/auth_app_routes.py
+++ Medical_record_management/apps/auth_app_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from jwt import encode
 from os import environ
@@ -25,48 +25,42 @@ def create_user(user_type):
             return jsonify({'message': 'Incorrect url.'})
         return jsonify({'message': f'New {user_type} user was created succesfully.'}), 200
     except IntegrityError as e:
-        return make_response(f"May be this {user_type} user exists in the database or you din't enter all the requerided values", 401)
+        return jsonify({'message': f"May be this {user_type} user exists in the database or you din't enter all the requerided values."})
 
 @auth_app.route("/login", methods=['POST'])
 def login():
     auth = request.authorization
-    if auth.username and auth.password:
-        user = get_user(auth=auth)
-        if user:
-            if check_password_hash(user.password, auth.password):
-                token = encode(
-                    {'public_id': user.public_id, 
-                    'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 
-                    environ.get('SECRET_KEY')
-                )
-                return jsonify({'token': token.decode('utf-8'),
-                                'user_name': user.name})
-            else:
-                return make_response('Incorrect password', 401)
-        else:
-            return make_response('User not found', 401)
+    user = get_user(auth=auth)
+    if not user:
+        return jsonify({'message': "User not found."})
     else:
-        return make_response('Must enter an username or password', 401)
+        if not check_password_hash(user.password, auth.password):
+            return jsonify({'message': "Incorrect password."})
+        else:
+            token = encode({'public_id': user.public_id, 
+                            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, 
+                            environ.get('SECRET_KEY'))
+            return jsonify({'token': token.decode('utf-8'), 'user_name': user.name})
 
-@auth_app.route("/user/hospital/create_doctor", methods=['POST'])
+@auth_app.route("/hospital/create_doctor", methods=['POST'])
 @token_required
 def create_doctor_user(current_user):
     if not current_user.user_type == 'hospital':
-        return make_response("You don't have permissions to perform this task.", 401)
+        return jsonify({'message': "You don't have permissions to perform this task."})
     elif not current_user.is_verificated:
         return jsonify({'message': 'You must verify your account. Please check your email imbox.'})
     else:
         try:
             create_doctor('doctor')
-            return jsonify({'message':'New doctor user was created successfully.'}), 200
+            return jsonify({'message': 'New doctor user was created successfully.'}), 200
         except IntegrityError:
-            return make_response(f'This doctor user exists in the database', 401)
+            return return jsonify({'message': 'This doctor user exists in the database.'})
 
 @auth_app.route('/user/account_confirmation/<public_id>', methods=['PUT'])
 def account_confirmation(public_id):
     user = get_user(public_id=public_id)
     if not user:
-        return jsonify({'message' : "This user doen's exist"})
+        return jsonify({'message' : "This user doen's exist."})
     else:
         user.is_verificated = True
         save()
@@ -78,7 +72,7 @@ def change_password(current_user):
     data = request.get_json()
     new_password = data.get('password')
     if check_password_hash(current_user.password, new_password):
-        return make_response('The new password matches with the old one', 401)
+        return jsonify({'message' : 'The new password matches with the old one.'})
     else:
         current_user.password = generate_password_hash(new_password, method='sha256')
         if current_user.user_type == 'doctor' and not current_user.password_changed:
@@ -90,12 +84,12 @@ def change_password(current_user):
 @token_required
 def medical_register(current_user):
     if not current_user.user_type == 'doctor':
-        return make_response("You don't have permissions to perform this task.", 401)
+        return jsonify({'message': "You don't have permissions to perform this task."})
     else:        
         if not current_user.is_verificated:
             return jsonify({'message': 'You must verify your account. Please check your email imbox.'})
         elif not current_user.password_changed:
-            return jsonify({'message': 'You must change your password. '})
+            return jsonify({'message': 'You must change your password. })
         else:
             doctor_specialities = get_doctor_specialities(current_user.id)
             print(doctor_specialities)
@@ -113,7 +107,7 @@ def consult_medical_record(current_user):
     if not current_user.is_verificated:
         return jsonify({'message': 'You must verify your account. Please check your email imbox.'})
     if current_user.user_type == 'doctor' and not current_user.password_changed:
-        return jsonify({'message': 'You must change your password. '})
+        return jsonify({'message': 'You must change your password.'})
     medical_records = get_medical_records(current_user)
     if medical_records:
         return jsonify({'data': medical_records})
